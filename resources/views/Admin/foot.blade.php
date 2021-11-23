c29ae5da92443ee63752dd246d959086949f281b
suga <lagustinus211@gmail.com>
2021-11-23 17:57:47 +0700

    add malam ini

--- resources/views/Admin/foot.blade.php
+++ resources/views/Admin/foot.blade.php
@@ -4,6 +4,7 @@
               <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © PKB 2021</span>
             </div>
           </footer>
+          {{-- sa --}}
           <!-- partial -->
 
             <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.5/js/plugins/piexif.min.js" type="text/javascript"></script>
