01c1cdfa369feac6eb8733631982a35ec6b9d69f
suga <lagustinus211@gmail.com>
2021-11-27 19:23:19 +0700

    addr

--- resources/views/DataDiri/resume.blade.php
+++ resources/views/DataDiri/resume.blade.php
@@ -4,15 +4,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <style type="text/css">
-      body{
+/*      body{
         margin: 0;
         padding: 0;
       }
-
+*/
       .kartu{
         margin: 20px;
-        width: 100vh;
-        height: 110vh;
+        width: 100vw;
+        height: 100%;
         border: 1px solid;
       }
 
